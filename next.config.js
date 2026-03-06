de1a6a1f8d94d0b6307780b046dbeedf2399b207
kleos <36254493+amlitio@users.noreply.github.com>
2026-03-06 03:00:18 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // This ensures the app works correctly when deployed to Vercel's edge network
+  swcMinify: true,
+}
 
+module.exports = nextConfig
